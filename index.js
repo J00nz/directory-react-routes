871eb768bf1df431de4c815cc896c72f06081af2
Johan Jönsson <joonz.zwy@gmail.com>
2017-03-15 11:06:44 +0100

    Fixed another issue with common-js interface.

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 var React = require('react');
 var Route = require('react-router').Route
 
-export default function DirectoryToReactRoutes(req, filter, routeProps) {
-  
+module.exports = function DirectoryToReactRoutes(req, filter, routeProps) {
   var pageComponents = {}
-  
-  req.keys().forEach(function(key) {
-    let newKey = key.replace("./", "").replace(".jsx", "").replace(".js", "")
 
-    let component = req(key)
-    
+  req.keys().forEach(function(key) {
+    var newKey = key.replace("./", "").replace(".jsx", "").replace(".js", "")
+
+    var component = req(key)
+
     if (filter && !filter(component))
       return
 
     pageComponents[newKey] = component
   })
-  
+
   var routes = Object.keys(pageComponents).map(function(i) {
     var path = "/" + i.replace(/\/index$/, '');
-    
+
     var props = {
       key: i, 
       path: path + "(/**)",
       component: pageComponents[i].default,
       base: path,
     };
-    
+
     Object.assign(props, routeProps);
-    
+
     return React.createElement(Route, props);
   })
-  
+
   return routes
 }
